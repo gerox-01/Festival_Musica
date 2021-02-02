b8ca10e9bac94a05aaede40ab56885966cd18041
Geronimo <gedaquito@gmail.com>
2021-02-02 12:00:33 -0500

    Se empezó a trabajar con SASS

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,34 @@
-const { series }= require('gulp'); //Require para importar y exports para exportar 
-function hola() {
-    console.log('Hola mundo');
+const { series, src, dest, watch }= require('gulp'); //Require para importar y exports para exportar 
+const sass = require('gulp-sass');
+
+//Funcion que compile SASS
+function css() {
+    return src ('src/scss/app.scss')
+        .pipe(sass({
+            outputStyle: 'expanded'
+        }))
+        .pipe(dest('./build/css'))
+
 }
-hola();
+function watchArchivos() {
+    watch('src/scss/app.scss', css)
+}
+exports.css = css;
+exports.watchArchivos = watchArchivos;
+//Practica o inducción acerca de la funcionalidad de gulp que es mediante funciones de js y sobre serie (ejecuta las funciones en orden) y parallel (ejeucta las funciones por tiempo vs proceso)
+// function css(done) {
+//     console.log('Compilando CSS...');
+//     done();
+// }
 
+// function javascript(done) {
+//     console.log('Compilando. JS..')
+//     done();
+// }
 
-exports.hola = hola;
+// function minificarHTML(done) {
+//     console.log('Minificando...')
+//     done();
+// }
+// // exports.default = series(css, javascript, minificarHTML);
+// exports.default = parallel(css, javascript, minificarHTML);
