758139d3e60d13f0cdb00db97dc57fad8281f428
Geronimo <gedaquito@gmail.com>
2021-02-06 06:24:01 -0500

    Se estuvo finalizando el proyecto

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,14 @@ const sass = require('gulp-sass');
 const imagemin = require('gulp-imagemin');
 const notify = require('gulp-notify');
 const webp = require('gulp-webp');
+const concat = require('gulp-concat');
 
 //Funcion que compile SASS
 
 const path = {
     imagenes: 'src/img/**/*',
     scss: 'src/scss/**/*.scss',
+    js: 'src/js/**/*.js',
 }
 
 function css() {
@@ -28,6 +30,12 @@ function minificarcss() {
     .pipe( dest('./build/css'))
 }
 
+function javascript() {
+    return src(path.js)
+    .pipe ( concat('bundle.js'))
+    .pipe ( dest('./build/js'))
+}
+
 function imagenes() {
     return src(path.imagenes)
     .pipe( imagemin())
@@ -43,13 +51,14 @@ function versionWebp() {
 }
 
 function watchArchivos() {
-    watch(path.scss, css)//* = La carpeta - ** = Todo
+    watch(path.scss, css);//* = La carpeta - ** = Todo
+    watch(path.js, javascript);
 }
 exports.css = css;
 exports.minificarcss = minificarcss;
 exports.imagenes = imagenes;
 exports.watchArchivos = watchArchivos;
-exports.default = series (css, imagenes, versionWebp ,watchArchivos);
+exports.default = series (css, javascript, imagenes, versionWebp ,watchArchivos);
 //Practica o inducción acerca de la funcionalidad de gulp que es mediante funciones de js y sobre serie (ejecuta las funciones en orden) y parallel (ejeucta las funciones por tiempo vs proceso)
 // function css(done) {
 //     console.log('Compilando CSS...');
