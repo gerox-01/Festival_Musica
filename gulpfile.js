8b6262f38e866991ce39d1bc193e263931f446a4
Geronimo <gedaquito@gmail.com>
2021-02-05 23:24:31 -0500

    Se esta desarrollando el index

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,55 @@
 const { series, src, dest, watch }= require('gulp'); //Require para importar y exports para exportar 
 const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+const notify = require('gulp-notify');
+const webp = require('gulp-webp');
 
 //Funcion que compile SASS
+
+const path = {
+    imagenes: 'src/img/**/*',
+    scss: 'src/scss/**/*.scss',
+}
+
 function css() {
-    return src ('src/scss/app.scss')
+    return src (path.scss)
         .pipe(sass({
             outputStyle: 'expanded'
         }))
         .pipe(dest('./build/css'))
 
 }
+
+function minificarcss() {
+    return src(path.scss)
+    .pipe( sass({
+        outputStyle: 'compressed'
+    }))
+    .pipe( dest('./build/css'))
+}
+
+function imagenes() {
+    return src(path.imagenes)
+    .pipe( imagemin())
+    .pipe( dest('./build/img'))
+    .pipe( notify({ message: 'Imagenes Minificadas'}));
+}
+
+function versionWebp() {
+    return src(path.imagenes)
+    .pipe (webp () )
+    .pipe( dest('./build/img'))
+    .pipe( notify({ message: 'Version WEBP lista'}))
+}
+
 function watchArchivos() {
-    watch('src/scss/app.scss', css)
+    watch(path.scss, css)//* = La carpeta - ** = Todo
 }
 exports.css = css;
+exports.minificarcss = minificarcss;
+exports.imagenes = imagenes;
 exports.watchArchivos = watchArchivos;
+exports.default = series (css, imagenes, versionWebp ,watchArchivos);
 //Practica o inducción acerca de la funcionalidad de gulp que es mediante funciones de js y sobre serie (ejecuta las funciones en orden) y parallel (ejeucta las funciones por tiempo vs proceso)
 // function css(done) {
 //     console.log('Compilando CSS...');
